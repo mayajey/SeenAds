0cde46a61c87cb1a4f8b88a1430714033ce3da9a
iperson98 <amade002@ucr.edu>
2017-07-18 15:56:37 -0700

    Fixed Again

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -39,17 +39,17 @@ public final class R {
         public static final int zOrderOnTop = 0x7f0100bd;
     }
     public static final class color {
-        public static final int common_google_signin_btn_text_dark = 0x7f0a0058;
-        public static final int common_google_signin_btn_text_dark_default = 0x7f0a0013;
-        public static final int common_google_signin_btn_text_dark_disabled = 0x7f0a0014;
-        public static final int common_google_signin_btn_text_dark_focused = 0x7f0a0015;
-        public static final int common_google_signin_btn_text_dark_pressed = 0x7f0a0016;
-        public static final int common_google_signin_btn_text_light = 0x7f0a0059;
-        public static final int common_google_signin_btn_text_light_default = 0x7f0a0017;
-        public static final int common_google_signin_btn_text_light_disabled = 0x7f0a0018;
-        public static final int common_google_signin_btn_text_light_focused = 0x7f0a0019;
-        public static final int common_google_signin_btn_text_light_pressed = 0x7f0a001a;
-        public static final int common_google_signin_btn_tint = 0x7f0a005a;
+        public static final int common_google_signin_btn_text_dark = 0x7f0a006f;
+        public static final int common_google_signin_btn_text_dark_default = 0x7f0a0015;
+        public static final int common_google_signin_btn_text_dark_disabled = 0x7f0a0016;
+        public static final int common_google_signin_btn_text_dark_focused = 0x7f0a0017;
+        public static final int common_google_signin_btn_text_dark_pressed = 0x7f0a0018;
+        public static final int common_google_signin_btn_text_light = 0x7f0a0070;
+        public static final int common_google_signin_btn_text_light_default = 0x7f0a0019;
+        public static final int common_google_signin_btn_text_light_disabled = 0x7f0a001a;
+        public static final int common_google_signin_btn_text_light_focused = 0x7f0a001b;
+        public static final int common_google_signin_btn_text_light_pressed = 0x7f0a001c;
+        public static final int common_google_signin_btn_tint = 0x7f0a0071;
     }
     public static final class drawable {
         public static final int common_full_open_on_phone = 0x7f020054;
