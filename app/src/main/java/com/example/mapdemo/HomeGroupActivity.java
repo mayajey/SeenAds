327acc058129430cb89cdc047065eee2378d7850
Taylor Jean-Jacques <tjeanjacques@fb.com>
2017-07-17 17:35:26 -0700

    login/group activity work

--- app/src/main/java/com/example/mapdemo/HomeGroupActivity.java
+++ app/src/main/java/com/example/mapdemo/HomeGroupActivity.java
@@ -1,4 +1,46 @@
 package com.example.mapdemo;
 
-public class HomeGroupActivity {}
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class HomeGroupActivity extends AppCompatActivity {
+    static final String TAG = HomeGroupActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homegroup_activity);
+        // User login
+        if (ParseUser.getCurrentUser() != null) { // start with existing user
+            startWithCurrentUser();
+        } else { // If not logged in, login as a new anonymous user
+            login();
+        }
+    }
+
+    // Get the userId from the cached currentUser object
+    void startWithCurrentUser() {
+        // TODO:
+    }
+
+    // Create an anonymous user using ParseAnonymousUtils and set sUserId
+    //TODO: change this to not anonymous
+    void login() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Anonymous login failed: ", e);
+                } else {
+                    startWithCurrentUser();
+                }
+            }
+        });
+    }
+}
